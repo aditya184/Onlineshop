07b4c3b4c985c97211eb5d2eca039f70a4b7b578
aditya184 <joshiaditya184@gmail.com>
2017-04-15 12:08:31 +0530

    Commit-My New project

---
+++ SportsMarketFrontend/src/main/java/com/aditya/controller/PageController.java
@@ -0,0 +1,16 @@
+package com.aditya.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController
+{
+    @RequestMapping(value="/")
+	public ModelAndView index()
+   {
+	   ModelAndView mv=new ModelAndView("page");
+		return mv;
+   }
+}
